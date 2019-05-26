952142faced4c0c329058158ad00648f53c1ad05
FERNANDO SILVA <fernando.silva236@fatec.sp.gov.br>
2019-05-26 19:42:00 -0300

    Parameterized tests

--- TestNinja/TestNinja.UnitTests/MathTests.cs
+++ TestNinja/TestNinja.UnitTests/MathTests.cs
@@ -22,26 +22,14 @@ namespace TestNinja.UnitTests
         }
 
         [Test]
-        public void Max_FirstArgumentIsGreater_ReturnsTheFirstArgument()
+        [TestCase(2,1,2)]
+        [TestCase(1,2,2)]
+        [TestCase(2,2,2)]
+        public void Max_WhenCalled_ReturnTheGreaterArgument(int a, int b, int expectedResult)
         {
 
-            var result = _math.Max(2, 1);
-            Assert.That(result, Is.EqualTo(2));
-        }
-
-        [Test]
-        public void Max_SecondArgumentIsGreater_ReturnsTheSecondArgument()
-        {
-
-            var result = _math.Max(1, 2);
-            Assert.That(result, Is.EqualTo(2));
-        }
-
-        [Test]
-        public void Max_ArgumentsAreEqual_ReturnTheSameArgument()
-        {
-            var result = _math.Max(2, 2);
-            Assert.That(result, Is.EqualTo(2));
+            var result = _math.Max(a, b);
+            Assert.That(result, Is.EqualTo(expectedResult));
         }
     }
 }
