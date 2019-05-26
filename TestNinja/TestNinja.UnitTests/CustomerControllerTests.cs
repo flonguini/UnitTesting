01756c6d7f9b5a34094a09e5fe3b72e29f53d988
FERNANDO SILVA <fernando.silva236@fatec.sp.gov.br>
2019-05-26 20:36:16 -0300

    Test GetCustomer method from CustomerController class

---
+++ TestNinja/TestNinja.UnitTests/CustomerControllerTests.cs
@@ -0,0 +1,39 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestNinja.Fundamentals;
+
+namespace TestNinja.UnitTests
+{
+    [TestFixture]
+    public class CustomerControllerTests
+    {
+
+        [Test]
+        public void GetCustomer_IdIsZero_ReturnNotFound()
+        {
+            var controller = new CustomerController();
+            var result = controller.GetCustomer(0);
+            // Not Found
+            Assert.That(result, Is.TypeOf<NotFound>());
+
+            // Not Found or one of its derivatives
+            //Assert.That(result, Is.InstanceOf<NotFound>());
+        }
+
+        [Test]
+        public void GetCustomer_IdIsNotZero_ReturnOkay()
+        {
+            var controller = new CustomerController();
+            var result = controller.GetCustomer(1);
+            // Not Found
+            Assert.That(result, Is.TypeOf<Ok>());
+
+            // Not Found or one of its derivatives
+            //Assert.That(result, Is.InstanceOf<NotFound>());
+        }
+    }
+}
