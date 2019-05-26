73b9646bba10cd18e93889266e6a65769c3dfa5b
FERNANDO SILVA <fernando.silva236@fatec.sp.gov.br>
2019-05-26 19:38:24 -0300

    Initialize the Math object with setup method

--- TestNinja/TestNinja.UnitTests/MathTests.cs
+++ TestNinja/TestNinja.UnitTests/MathTests.cs
@@ -6,41 +6,41 @@ namespace TestNinja.UnitTests
     [TestFixture]
     public class MathTests
     {
+        private Math _math;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _math = new Math();
+        }
+
         [Test]
         public void Add_WhenCalled_ReturnTheSumOfArguments()
         {
-            var math = new Math();
-            var result = math.Add(1, 2);
+            var result = _math.Add(1, 2);
             Assert.That(result, Is.EqualTo(3));
         }
 
         [Test]
         public void Max_FirstArgumentIsGreater_ReturnsTheFirstArgument()
         {
-            var math = new Math();
 
-            var result = math.Max(2, 1);
-
+            var result = _math.Max(2, 1);
             Assert.That(result, Is.EqualTo(2));
         }
 
         [Test]
         public void Max_SecondArgumentIsGreater_ReturnsTheSecondArgument()
         {
-            var math = new Math();
 
-            var result = math.Max(1, 2);
-
+            var result = _math.Max(1, 2);
             Assert.That(result, Is.EqualTo(2));
         }
 
         [Test]
         public void Max_ArgumentsAreEqual_ReturnTheSameArgument()
         {
-            var math = new Math();
-
-            var result = math.Max(2, 2);
-
+            var result = _math.Max(2, 2);
             Assert.That(result, Is.EqualTo(2));
         }
     }
