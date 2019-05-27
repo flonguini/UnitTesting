c0642ccc81a18f980af6681a1f8c7a1904e81a9d
FERNANDO SILVA <fernando.silva236@fatec.sp.gov.br>
2019-05-27 00:04:37 -0300

    Third scenario and bug fix

--- TestNinja/TestNinja.UnitTests/Mocking/BookingHelperTests.cs
+++ TestNinja/TestNinja.UnitTests/Mocking/BookingHelperTests.cs
@@ -55,6 +55,19 @@ namespace TestNinja.UnitTests.Mocking
             Assert.That(result, Is.EqualTo("a"));
         }
 
+        [Test]
+        public void OverlappingBookingExists_BookingStartsBeforeAndFinishesAfterAndExistingBooking_ReturnExistingBookings()
+        {
+            var result = BookingHelper.OverlappingBookingsExist(new Booking
+            {
+                Id = 1,
+                ArrivalDate = Before(_existingBooking.ArrivalDate),
+                DepartureDate = After(_existingBooking.DepartureDate)
+            }, _repository.Object);
+
+            Assert.That(result, Is.EqualTo(_existingBooking.Reference));
+        }
+
         private DateTime Before(DateTime dateTime, int days = 1)
         {
             return dateTime.AddDays(-days);
