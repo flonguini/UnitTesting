8bd4ea7a99134039d74b1d64f1906a7efa0eb12f
FERNANDO SILVA <fernando.silva236@fatec.sp.gov.br>
2019-05-26 23:36:09 -0300

    Test OverlappingBookingExsist method from BookingHelper class

--- TestNinja/TestNinja.UnitTests/Mocking/BookingHelperTests.cs
+++ TestNinja/TestNinja.UnitTests/Mocking/BookingHelperTests.cs
@@ -1,4 +1,9 @@
-﻿using NUnit.Framework;
+﻿using Moq;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using TestNinja.Mocking;
 
 namespace TestNinja.UnitTests.Mocking
 {
@@ -8,7 +13,28 @@ namespace TestNinja.UnitTests.Mocking
         [Test]
         public void OverlappingBookingExists_BookingStartsAndFinishesBeforesAndExistingBooking_ReturnEmptyString()
         {
+            var mock = new Mock<IBookingRepository>();
+            mock.Setup(r => r.GetActiveBooks(1)).Returns(new List<Booking>
+            {
+                new Booking
+                {
+                    Id = 2,
+                    ArrivalDate = new DateTime(2017,1,15,14,0,0),
+                    DepartureDate = new DateTime(2017,1,20,10,0,0),
+                    Reference = "a"
 
+                }
+
+            }.AsQueryable());
+
+            var result = BookingHelper.OverlappingBookingsExist(new Booking
+            {
+                Id = 1,
+                ArrivalDate = new DateTime(2017, 1, 10, 14, 0, 0),
+                DepartureDate = new DateTime(2017, 1, 14, 10, 0, 0)
+            }, mock.Object);
+
+            Assert.That(result, Is.Empty);
         }
     }
 }
